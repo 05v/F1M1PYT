dd68b2a5f96cf2415d7b43a3d4f33c4834a5b739
vladverheij <vladverheij@gmail.com>
2021-09-16 13:03:11 +0200

    Added stel_vragen.py

---
+++ PYTB1L2InterestingTellMeMore/stel_vragen.py
@@ -0,0 +1,8 @@
+
+print("[x] Hallo, ik ben Vlad")
+name = input("[?] Wat is jou naam?: ")
+print("[x] Hallo", name)
+age = input("[?] Hoe oud ben jij: ")
+print("[x] Cool! je bent", age, ", ik ben 15 :) ")
+month_born = input("[?] In welke maand ben jij geboren?: ")
+print("[x] Dus", month_born, "? Ik in November!")
