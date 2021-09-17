2911db61835921e6f52fb86002ff3fce4fcce79d
vladverheij <vladverheij@gmail.com>
2021-09-17 11:59:39 +0200

    added PYTB1L2FamliyTies folder + file

---
+++ PYTB1L2FamliyTies/FamilyTies.py
@@ -0,0 +1,5 @@
+name = input("Wat is jou naam: ")
+age = input("Wat is jou leeftijd: ")
+distance = ("27.5km")
+
+print("Hallo ik ben", name, "ik ben", age, "jaar oud", "en ik woon ", distance, "van school.") 
